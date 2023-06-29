59a0465348cd7364fd8a69071f72c70e0429b61c
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-29 12:10:56 +0700

    currentNode = root

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -57,6 +57,12 @@ public:
 
 	void search(string element, Node*& parent, Node*& currentNode)
 	{
-		
+		//This function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+
+		}
 	}
 };
