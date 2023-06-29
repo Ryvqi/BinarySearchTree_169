5c268098bcf8aadc079b6d5643bac45fe0556d19
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-29 11:51:39 +0700

    Node* parent

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -35,6 +35,7 @@ public:
 		newNode->leftchild = NULL; // Make the left child of the new node point to NULL
 		newNode->rightchild = NULL; //Make the right child of the new node point to NULL
 
-
+		Node* parent = NULL;
+		Node* currentNode = NULL;
 	}
 };
